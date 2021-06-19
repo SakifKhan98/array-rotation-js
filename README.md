d1d83fe4e1df5d3e4975d00f94504f380530eb0d
SakifKhan98 <sakifkhan98@gmail.com>
2021-06-20 03:38:08 +0600

    Completed array element shifting to left problem

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # array-rotation-js
+
+##This is a simple javascript programme to shift array elements to left
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const rotateLeft = (arr, rotations) => {
-  //   const rotatedArray = arr.concat();
-  for (let i = 0; i < rotations; i++) {
+// rotateLeft has following parameters:
+// n = size of array
+// d = amount of rotations
+// arr[n] is an array
+
+const rotateLeft = (n, d, arr) => {
+  for (let i = 0; i < d; i++) {
     const frontItem = arr.shift();
     arr.push(frontItem);
   }
@@ -8,10 +12,11 @@ const rotateLeft = (arr, rotations) => {
   return arr;
 };
 
-const numRotations = 4;
-const sampleArray = [1, 2, 3, 4, 5];
+const arraySize = 5;
+const numOfRotations = 4;
+const array = [1, 2, 3, 4, 5];
 
 const expectedOutput = [5, 1, 2, 3, 4];
 
-console.log(rotateLeft(sampleArray, numRotations));
-// console.log(expectedOutput);
+const rotatedArray = rotateLeft(arraySize, numOfRotations, array);
+console.log(rotatedArray);
